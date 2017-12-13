9086a0172fe5cb8f297da8feff2949834520f9c2
Sydney <sfpugh@loyola.edu>
2017-12-13 06:39:06 -0500

    circle program

---
+++ RunIt_Circle.java
@@ -0,0 +1,15 @@
+
+public class RunIt {
+public static void main(String[] args) {
+
+
+    // Create a Circle object passing in user input
+    Circle circle = new Circle(5.0);
+
+    // Display circle information
+    System.out.println("Area is " + circle.getArea());
+    System.out.println("Diameter is " + circle.getDiameter());
+    System.out.println("Circumference is " + circle.getCircumference());
+
+}
+}
